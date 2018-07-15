f9d6501175d2f2ad17c9d77771af87b5cdb3ca73
Douglas Martin <mdgeek1@gmail.com>
2018-07-15 01:09:28 -0400

    Set up travis-ci.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_BRANCH" = 'master' ]] || [[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+\.x$ ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
-  openssl aes-256-cbc -K $encrypted_288d6fbfec96_key -iv $encrypted_288d6fbfec96_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+  openssl aes-256-cbc -K $encrypted_ccecb673e031_key -iv $encrypted_ccecb673e031_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
   gpg --fast-import travis/codesigning.asc
   mvn -V -B -s travis/settings.xml clean deploy -P sign -DskipTests
 else
