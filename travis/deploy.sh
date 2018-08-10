417428bada1713de1a472652e40d44197f2efe24
Douglas Martin <mdgeek1@gmail.com>
2018-08-10 09:05:59 -0400

    Update deploy script.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-if [[ "$TRAVIS_BRANCH" = 'master' ]] || [[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+\.x$ ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+
+if [[ "$TRAVIS_PULL_REQUEST" != 'false' ]]; then
+  exit 0
+fi
+
+if [[ "$TRAVIS_BRANCH" = 'master' ]]; then
+  echo "Deploying branch $TRAVIS_BRANCH..."
+  mvn -V -B -s travis/settings.xml clean deploy -DskipTests
+  exit 0
+fi
+
+if [[ "$TRAVIS_BRANCH" =~ ^[0-9]+(\.[0-9]+)*$ ]]; then
+  echo "Deploying branch $TRAVIS_BRANCH..."
   openssl aes-256-cbc -K $encrypted_ccecb673e031_key -iv $encrypted_ccecb673e031_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
   gpg --fast-import travis/codesigning.asc
   mvn -V -B -s travis/settings.xml clean deploy -P sign -DskipTests
-else
-  echo "Branch $TRAVIS_BRANCH not eligible for deployment."
+  exit 0
 fi
+
+echo "Branch $TRAVIS_BRANCH not eligible for deployment."
