b24dd7c4cee263ed92c79e29ecc0be03e704148e
Douglas Martin <mdgeek1@gmail.com>
2019-09-28 09:36:31 -0400

    Add "about" property to SMART manifest

--- fcf-fhir-smart/fcf-fhir-smart-common/src/main/java/org/fujionclinical/fhir/smart/common/SmartContainer.java
+++ fcf-fhir-smart/fcf-fhir-smart-common/src/main/java/org/fujionclinical/fhir/smart/common/SmartContainer.java
@@ -28,7 +28,11 @@ package org.fujionclinical.fhir.smart.common;
 import org.apache.commons.lang.StringUtils;
 import org.fujion.annotation.Component;
 import org.fujion.component.BaseUIComponent;
+import org.fujion.component.Page;
 import org.fujionclinical.fhir.smart.common.SmartContextBase.ContextMap;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.Assert;
 
 import java.util.*;
 
@@ -45,11 +49,17 @@ public class SmartContainer extends BaseUIComponent implements ISmartContextSubs
 
     private final SmartManifest _manifest = new SmartManifest();
 
+    private final Set<String> _contexts = new HashSet<>();
+
     private final Map<String, ContextMap> _context = new HashMap<>();
 
     private final SmartContextRegistry contextRegistry;
 
     private final SmartMessageBroker messagingService;
+
+    private boolean initialized;
+
+    private String aboutSrc;
 
     private String src;
 
@@ -74,7 +84,7 @@ public class SmartContainer extends BaseUIComponent implements ISmartContextSubs
             src = requestUrl + "/" + src;
         }
 
-        propertyChange("src", this.src, this.src = src, true);
+         propertyChange("src", this.src, this.src = src, true);
     }
 
     @Override
@@ -83,13 +93,40 @@ public class SmartContainer extends BaseUIComponent implements ISmartContextSubs
         props.put("wclazz", "fcf_smart_container");
     }
 
+    @Override
+    protected void onAttach(Page page) {
+        super.onAttach(page);
+        String about = _manifest.getValue("about");
+
+        if (about == null) {
+            about = _manifest.getValue("description");
+        }
+
+        if (about != null) {
+            String aboutPath = UUID.randomUUID().toString() + ".html";
+            Resource resource = new ByteArrayResource(about.getBytes());
+            aboutSrc = page.registerResource(aboutPath, resource);
+            refresh();
+        }
+    }
+
     /**
      * Sets the manifest.
      *
      * @param manifest The SMART manifest.
      */
     public void setManifest(SmartManifest manifest) {
+        Assert.isTrue(!initialized, "A SMART manifest has already been set.");
+        initialized = true;
         _manifest.init(manifest);
+        String contextStr = _manifest.getValue("context");
+        _contexts.add("user");
+
+        if (contextStr != null) {
+            String[] contextArray = contextStr.replaceAll("\\s", "").split("\\,");
+            _contexts.addAll(Arrays.asList(contextArray));
+        }
+
         subscribeAll(true);
     }
 
@@ -133,9 +170,25 @@ public class SmartContainer extends BaseUIComponent implements ISmartContextSubs
         subscribeAll(false);
     }
 
+    /**
+     * Refreshes the container's SMART app.
+     */
     public void refresh() {
         setSrc(null);
-        setSrc(getUrl());
+        setSrc(contextPrepared() ? getUrl() : aboutSrc);
+    }
+
+    /**
+     * Returns true only if all registered contexts are populated.
+     */
+    private boolean contextPrepared() {
+        for (String context: _contexts) {
+            if (!_context.containsKey(context)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -153,15 +206,8 @@ public class SmartContainer extends BaseUIComponent implements ISmartContextSubs
      * @param subscribe If true, subscribe; false, unsubscribe;
      */
     private void subscribeAll(boolean subscribe) {
-        String contextStr = _manifest == null ? null : _manifest.getValue("context");
-
-        if (contextStr != null) {
-            Set<String> contexts = new HashSet<>(Arrays.asList(contextStr.split("\\,")));
-            contexts.add("user");
-
-            for (String context : contexts) {
-                subscribe(context.trim(), subscribe);
-            }
+        for (String context : _contexts) {
+            subscribe(context, subscribe);
         }
     }
 
