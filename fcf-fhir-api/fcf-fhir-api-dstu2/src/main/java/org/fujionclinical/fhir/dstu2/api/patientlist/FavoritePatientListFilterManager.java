fc8d80f1ccaeaa16ae3acca36f61bde9630fd479
Douglas Martin <mdgeek1@gmail.com>
2020-05-06 00:41:43 -0400

    Update copyright date.

--- fcf-fhir-api/fcf-fhir-api-dstu2/src/main/java/org/fujionclinical/fhir/dstu2/api/patientlist/FavoritePatientListFilterManager.java
+++ fcf-fhir-api/fcf-fhir-api-dstu2/src/main/java/org/fujionclinical/fhir/dstu2/api/patientlist/FavoritePatientListFilterManager.java
@@ -2,7 +2,7 @@
  * #%L
  * Fujion Clinical Framework
  * %%
- * Copyright (C) 2019 fujionclinical.org
+ * Copyright (C) 2020 fujionclinical.org
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
