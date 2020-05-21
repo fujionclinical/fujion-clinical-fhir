8d89237f961ea34013ac3f998df4e8c79e6cc607
Douglas Martin <mdgeek1@gmail.com>
2020-05-21 12:06:25 -0400

    Tweak profiles.

--- fcf-fhir-demo/fcf-fhir-demo-webapp/build.sh
+++ fcf-fhir-demo/fcf-fhir-demo-webapp/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
 mvn clean package \
--Psecurity-h2 \
--Pfhir-dstu2 \
--Popencds \
--Pepic \
--Psmart \
+-Psecurity-h2,fhir-dstu2,opencds,epic,smart \
 -Dfhir-endpoint="http://localhost:9080/fhir-server-dstu2/fhir" \
 -Dcdshooks-endpoint="" \
 -Doauth-launch-binder-endpoint="http://localhost:9080/oauth-r4-demo/auth/Launch" \
